10c6271b6b3f4a86376deb64942021094f337557
fakynno <fakynno@gmail.com>
2022-06-25 18:30:17 -0300

    testando a aplicacao

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -17,13 +19,37 @@ public class Main {
         curso2.setDescricao("Curso introdutório de Angular");
         curso2.setCargaHoraria(6);
 
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("Mentoria de Java");
-        mentoria.setDescricao("mentoria guiada nos principais aspectos do java");
-        mentoria.setData(LocalDate.now());
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Angular");
+        mentoria1.setDescricao("mentoria guiada nos principais aspectos do Angular");
+        mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria de Java");
+        mentoria2.setDescricao("mentoria guiada nos principais aspectos do java");
+        mentoria2.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Mobile Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Mobile Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+        bootcamp.getConteudos().add(mentoria2);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Fabio Aquino");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdo em que " + dev1.getNome() + " está escrito: " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        System.out.println("Conteúdo concluído até o momento por " + dev1.getNome() + ": " + dev1.getConteudosConcluidos());
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Gabriel Santos");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdo em que " + dev2.getNome() + " está escrito: " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("Conteúdo concluído até o momento: " + dev2.getConteudosConcluidos());
+
     }
 }
