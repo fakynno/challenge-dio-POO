9c4650f80ca04af3dc87af3da6a8c6f033984547
fakynno <fakynno@gmail.com>
2022-06-25 11:53:20 -0300

    Usando dados para teste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Main {
+    public static void main(String[] args) throws DateTimeParseException {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Curso introdutório de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de Angular");
+        curso2.setDescricao("Curso introdutório de Angular");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("mentoria guiada nos principais aspectos do java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
